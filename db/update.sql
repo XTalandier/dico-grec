4441e117fa215542eb8e85c285a4d128b7ef157a
Xavier TALANDIER <xavier@talandier.fr>
2013-05-19 15:15:17 +0200

    Update db

--- db/update.sql
+++ db/update.sql
@@ -1,8 +1,8 @@
-UPDATE dico SET gr=REPLACE(gr, 'ἱ' , 'i');
-UPDATE dico SET gr=REPLACE(gr, 'ἰ' , 'i');
-UPDATE dico SET gr=REPLACE(gr, 'ί' , 'i');
-UPDATE dico SET gr=REPLACE(gr, 'ὶ' , 'i');
-UPDATE dico SET gr=REPLACE(gr, 'ί' , 'i');
+UPDATE dico SET gr=REPLACE(gr, 'ἱ' , 'ι');
+UPDATE dico SET gr=REPLACE(gr, 'ἰ' , 'ι');
+UPDATE dico SET gr=REPLACE(gr, 'ί' , 'ι');
+UPDATE dico SET gr=REPLACE(gr, 'ὶ' , 'ι');
+UPDATE dico SET gr=REPLACE(gr, 'ί' , 'ι');
 
 UPDATE dico SET gr=REPLACE(gr, 'ἡ' , 'η');
 UPDATE dico SET gr=REPLACE(gr, 'ἠ' , 'η');
@@ -10,13 +10,11 @@ UPDATE dico SET gr=REPLACE(gr, 'ή' , 'η');
 UPDATE dico SET gr=REPLACE(gr, 'ὴ' , 'η');
 UPDATE dico SET gr=REPLACE(gr, 'ή' , 'η');
 
-
 UPDATE dico SET gr=REPLACE(gr, 'ἑ' , 'ε');
 UPDATE dico SET gr=REPLACE(gr, 'ἐ' , 'ε');
 UPDATE dico SET gr=REPLACE(gr, 'έ' , 'ε');
 UPDATE dico SET gr=REPLACE(gr, 'ὲ' , 'ε');
 UPDATE dico SET gr=REPLACE(gr, 'έ' , 'ε');
-
 
 UPDATE dico SET gr=REPLACE(gr, 'ὑ' , 'υ');
 UPDATE dico SET gr=REPLACE(gr, 'ὐ' , 'υ');
@@ -24,20 +22,17 @@ UPDATE dico SET gr=REPLACE(gr, 'ύ' , 'υ');
 UPDATE dico SET gr=REPLACE(gr, 'ὺ' , 'υ');
 UPDATE dico SET gr=REPLACE(gr, 'ύ' , 'υ');
 
-
 UPDATE dico SET gr=REPLACE(gr, 'ἁ' , 'α');
 UPDATE dico SET gr=REPLACE(gr, 'ἀ' , 'α');
 UPDATE dico SET gr=REPLACE(gr, 'ά' , 'α');
 UPDATE dico SET gr=REPLACE(gr, 'ὰ' , 'α');
 UPDATE dico SET gr=REPLACE(gr, 'ά' , 'α');
 
-
 UPDATE dico SET gr=REPLACE(gr, 'ὡ' , 'ω');
 UPDATE dico SET gr=REPLACE(gr, 'ὠ' , 'ω');
 UPDATE dico SET gr=REPLACE(gr, 'ώ' , 'ω');
 UPDATE dico SET gr=REPLACE(gr, 'ὼ' , 'ω');
 UPDATE dico SET gr=REPLACE(gr, 'ώ' , 'ω');
-
 
 UPDATE dico SET gr=REPLACE(gr, 'ὁ' , 'ο');
 UPDATE dico SET gr=REPLACE(gr, 'ὀ' , 'ο');
